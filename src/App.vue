eb20aff5fe7d17e519572139e93f6a68672bd244
mxlgrrr <106144699+mxlgrrr@users.noreply.github.com>
2023-05-11 22:52:56 +0200

    watcher actualizado

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,18 @@
   import { storeToRefs } from "pinia";
   import { useUserStore } from "./stores/user.js";
   import { useRouter } from "vue-router";
-  import { RouterView } from "vue-router";
   import NavBar from "./components/NavbarComponent.vue";
 
-  const router = useRouter();
   const userStore = useUserStore();
   const { user } = storeToRefs(userStore);
+  const router = useRouter();
 
-  watch(user, () => router.currentRoute, async () => {
-    try {
-      await userStore.fetchUser();
-      const isAuthenticated = Boolean(user.value);
-      if (!isAuthenticated && router.currentRoute.name !== "home") {
-        router.push({ name: "home" });
-      } else if (isAuthenticated && router.currentRoute.name !== "tasks") {
-        router.push({ name: "tasks" });
-      }
-    } catch (e) {
-      console.log(e);
+  watch(user, () => {
+    const isAuthenticated = Boolean(user.value);
+    if (!isAuthenticated && router.currentRoute.name !== "home") {
+      router.push({ name: "home" });
+    } else if (isAuthenticated && router.currentRoute.name !== "tasks") {
+      router.push({ name: "tasks" });
     }
   });
 
@@ -37,21 +31,21 @@
       console.log(e);
     }
   });
-
 </script>
 
 <style>
-    html {
-      font-family: "Roboto", sans-serif;
-    }
-    body,
-    html, 
-    #app {
-      height: 100%;
-    }
+  html {
+    font-family: "Roboto", sans-serif;
+  }
+
+  body,
+  html,
+  #app {
+    height: 100%;
+  }
 </style>
 
 <template>
   <NavBar></NavBar>
-  <RouterView />
+  <router-view />
 </template>
